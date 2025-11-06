07ed94fbe3569b4a92537124b74eb4f86debca6e
albert-dimitrov <dimitrov.albert420@gmail.com>
2025-11-06 15:11:29 +0200

    Fix: fix related names of a Room model

--- team_task_manager/rooms/models.py
+++ team_task_manager/rooms/models.py
@@ -7,8 +7,8 @@ User = get_user_model()
 class Room(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
-    leaders = models.ForeignKey(User, on_delete=models.CASCADE, related_name='leaders')
-    members = models.ManyToManyField(User, related_name='members', blank=True)
+    leaders = models.ForeignKey(User, on_delete=models.CASCADE, related_name='led_rooms')
+    members = models.ManyToManyField(User, related_name='rooms', blank=True)
 
     def __str__(self):
         return self.name
